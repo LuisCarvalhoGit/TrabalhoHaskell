3f66e4c9b08951e46c91fe37ecf14258f77e6485
Luis Carvalho <pro.luis.carvalho@gmail.com>
2024-10-23 11:44:05 +0100

    types

--- Comandos.hs
+++ Comandos.hs
@@ -10,17 +10,23 @@ module Comandos where
 -- estado -> (Localização, ligado) -> ((Float,Float,Float), Bool)
 -- movimento -> (dx, dy, dz) -> (Float,Float,Float)
 
-atualiza_acao :: (Bool, ((Float, Float, Float), Bool)) -> ((Float, Float, Float), Bool)
-atualiza_acao (ligado, (coord, _))
-    | ligado == True = (coord, True)
-    | ligado == False = (coord, False)
-    | otherwise = (coord, False) -- Se a ação for inválida
+-- Tipos Sinónimos
+type Coord = (Float, Float, Float)
+type Estado = (Coord, Bool)
+type Movimento = Coord
+--type Nave = ([Movimento], String)  -- Lista de movimentos e 
+--type ListaNaves = [(Nave, Estado)] -- List of (ship, initial state) pairs
 
-move :: (Float, Float, Float) -> ((Float, Float, Float), Bool) -> ((Float, Float, Float), Bool)
+atualiza_acao :: (Bool, Estado) -> Estado
+atualiza_acao (acao, (coord, _)) = (coord, acao)
+    
+
+move :: Movimento -> Estado -> Estado
 move (dx, dy, dz) ((x, y, z), ligado)
     | ligado == True = ((x + dx, y + dy, z + dz), ligado)
     | otherwise = ((x, y, z), ligado) -- A nave não se move se estiver desligada
 
-move_lista :: [(Float, Float, Float)] -> ((Float, Float, Float), Bool) -> ((Float, Float, Float), Bool)
+
+move_lista :: [Movimento] -> Estado -> Estado
 move_lista [] estado = estado
 move_lista (m : ms) estado = move_lista ms (move m estado)
