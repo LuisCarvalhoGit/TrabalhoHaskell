da5fb9f3c9a1dc921d9d1a5602557f16042b15d7
Luis Carvalho <pro.luis.carvalho@gmail.com>
2024-11-03 17:43:22 +0000

    fix

--- Comandos.hs
+++ Comandos.hs
@@ -14,45 +14,44 @@ module Comandos where
 type Coord = (Float, Float, Float)
 type Estado = (Coord, Bool)
 type Movimento = Coord
-type Nave = ([Movimento], String)  -- Lista de movimentos e ID da nave
-type ListaNaves = [(Nave, Estado)] -- Lista de pares de (Nave, Estado) 
+type Nave = ([Movimento], String) -- Lista de movimentos e ID da nave
+type ListaNaves = [(Nave, Estado)] -- Lista de pares de (Nave, Estado)
 
 atualiza_acao :: (Bool, Estado) -> Estado
 atualiza_acao (acao, (coord, _)) = (coord, acao)
-
 
 move :: Movimento -> Estado -> Estado
 move (dx, dy, dz) ((x, y, z), ligado)
     | ligado == True = ((x + dx, y + dy, z + dz), ligado)
     | otherwise = ((x, y, z), ligado) -- A nave não se move se estiver desligada
 
-
 move_lista :: [Movimento] -> Estado -> Estado
 move_lista [] estado = estado
 move_lista (m : ms) estado = move_lista ms (move m estado)
 
-
 move_varios :: [Nave] -> [Estado] -> [(Estado, String)]
 move_varios [] [] = [] -- Caso base
-move_varios ((movs, id):naves) (estado:estados) = (move_lista movs estado, id) : move_varios naves estados
+move_varios ((movs, id) : naves) (estado : estados) = (move_lista movs estado, id) : move_varios naves estados
 move_varios _ _ = [] -- Listas com tamanhos diferentes
 
+-- Função auxiliar que compara duas localizações
+compara_coord :: Coord -> Coord -> Bool
+compara_coord (x1, y1, z1) (x2, y2, z2) = x1 == x2 && y1 == y2 && z1 == z2
 
--- 5. Função que verifica embates entre naves
+-- Função que verifica embates entre naves
 verifica_embates :: Estado -> [Estado] -> Bool
-verifica_embates (novaLocalizacao, _) = any (\(localizacao, _) -> localizacao == novaLocalizacao)
-
+verifica_embates _ [] = False
+verifica_embates (loc1, _) ((loc2, _) : estados)
+    | compara_coord loc1 loc2 = True
+    | otherwise = verifica_embates (loc1, False) estados
 
 --  6.Crie uma nova versão, atualizada, da função move_varios que apenas move uma nave caso esse movimento não origine um embate.
 
 move_varios_atualizado :: [Nave] -> [Estado] -> [(Estado, String)]
 move_varios_atualizado [] [] = [] -- Caso base
-move_varios_atualizado ((movs, id):naves) (estado:estados) = 
+move_varios_atualizado ((movs, id) : naves) (estado : estados) =
     let novoEstado = move_lista movs estado
-        embate = verifica_embates (fst novoEstado) (map fst (naves ++ estados))
-    in if embate
-       then (estado, id) : move_varios naves estados -- Não move a nave se houver embate
-       else (novoEstado, id) : move_varios naves estados
+     in if verifica_embates novoEstado estados
+            then (estado, id) : move_varios_atualizado naves estados -- Não move a nave se houver embate
+            else (novoEstado, id) : move_varios naves estados -- Move a nave se não houver embate
 move_varios_atualizado _ _ = [] -- Listas com tamanhos diferentes
-
-
