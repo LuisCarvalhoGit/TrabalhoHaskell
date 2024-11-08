fc68879d8e95f0ed46eb81fdbfb4991f4ee6d9b8
Luis Carvalho <pro.luis.carvalho@gmail.com>
2024-11-08 00:06:19 +0000

    final (probably)

--- Comandos.hs
+++ Comandos.hs
@@ -1,8 +1,8 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
 {-# HLINT ignore "Redundant ==" #-}
 {-# HLINT ignore "Use foldl" #-}
 {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 module Comandos where
 
@@ -43,21 +43,54 @@ compara_coord (x1, y1, z1) (x2, y2, z2) = x1 == x2 && y1 == y2 && z1 == z2
 
 -- 5. Função que verifica embates entre naves
 verifica_embates :: Estado -> [Estado] -> Bool
-verifica_embates _ [] = False
-verifica_embates (loc1, _) ((loc2, _) : estados)
-    | compara_coord loc1 loc2 = True
-    | otherwise = verifica_embates (loc1, False) estados
+verifica_embates _ [] = False -- Caso base: sem outros estados para comparar, sem colisão
+verifica_embates (coord, _) ((loc, _) : estados) -- Se o estado atual colide com algum outro, retorna True
+    | compara_coord coord loc = True
+    | otherwise = verifica_embates (coord, False) estados
+
+-- Função auxiliar para extrair estados de [(Estado, String)]
+extract_estados :: [(Estado, String)] -> [Estado]
+extract_estados [] = [] -- Caso base
+extract_estados ((estado, _) : resto) = estado : extract_estados resto -- Extrai cada estado da lista de pares
+
+-- Função auxiliar que verifica se as listas têm o mesmo tamanho
+mesmo_tamanho :: [a] -> [b] -> Bool
+mesmo_tamanho [] [] = True -- Ambas as listas vazias, então têm o mesmo tamanho
+mesmo_tamanho (_ : xs) (_ : ys) = mesmo_tamanho xs ys -- Verifica recursivamente cada elemento
+mesmo_tamanho _ _ = False -- Se uma lista é vazia e a outra não, retorna False
 
 -- 6. Versão atualizada de move_varios que evita embates
-
 move_varios_atualizado :: [Nave] -> [Estado] -> [(Estado, String)]
-move_varios_atualizado [] _ = [] -- Caso lista de Naves vazia
-move_varios_atualizado _ [] = [] -- Caso lista de Estados vazia
-move_varios_atualizado ((movs, id) : naves) (estado : estados) =
-    let resultadoPrevio = move_varios_atualizado naves estados -- Processa o restante das naves recursivamente
-        estadosFinais = map fst resultadoPrevio -- Extrai os estados finais das naves após se moverem
-        novoEstado = move_lista movs estado
-    in if verifica_embates novoEstado estadosFinais
-        then (estado, id) : resultadoPrevio -- Não move a nave se houver colisão
-        else (novoEstado, id) : resultadoPrevio -- Move a nave se não houver colisão
-move_varios_atualizado _ _ = [] -- Caso de listas com diferentes tamanhos
+move_varios_atualizado [] _ = [] -- Caso base: sem naves para mover
+move_varios_atualizado _ [] = [] -- Caso base: sem estados fornecidos
+move_varios_atualizado naves estados
+    | not (mesmo_tamanho naves estados) = [] -- Se listas têm tamanhos diferentes, retorna lista vazia
+    | otherwise =
+        let processar_movimentos = processa_movimentos naves estados -- Calcula os novos estados aplicando movimentos
+            novos_estados = extract_estados processar_movimentos -- Extrai os novos estados para verificação de colisão
+            havera_colisao = verifica_colisoes novos_estados -- Verifica colisões
+         in if havera_colisao
+                then manter_estados_originais naves estados -- Se houver colisão, mantém estados antigos
+                else processar_movimentos -- Se não houver colisão, usa os novos estados
+  where
+    -- Função auxiliar que aplica movimentos a cada nave e seu estado original
+    processa_movimentos :: [Nave] -> [Estado] -> [(Estado, String)]
+    processa_movimentos [] [] = []
+    processa_movimentos ((movs, id) : ns) (est : es) = (move_lista movs est, id) : processa_movimentos ns es
+    processa_movimentos _ _ = [] -- Caso listas tenham tamanhos diferentes, retorna lista vazia
+
+    -- Função auxiliar que mantém os estados originais se houver colisão
+    manter_estados_originais :: [Nave] -> [Estado] -> [(Estado, String)]
+    manter_estados_originais [] [] = []
+    manter_estados_originais ((_, id) : ns) (est : es) = (est, id) : manter_estados_originais ns es
+    manter_estados_originais _ _ = [] -- Caso listas tenham tamanhos diferentes, retorna lista vazia
+
+    -- Extrai IDs de uma lista de Naves
+    extrair_ids :: [Nave] -> [String]
+    extrair_ids [] = []
+    extrair_ids ((_, id) : ns) = id : extrair_ids ns
+
+    -- Verifica colisões entre estados
+    verifica_colisoes :: [Estado] -> Bool
+    verifica_colisoes [] = False
+    verifica_colisoes (x : xs) = verifica_embates x xs || verifica_colisoes xs
--- Main.hs
+++ Main.hs
@@ -10,21 +10,22 @@ runTest name result = putStrLn $ name ++ ": " ++ if result then "PASSED" else "F
 testMoveVariosAtualizado :: IO ()
 testMoveVariosAtualizado = do
     -- Test 1: Basic movement without collision
-    let nave1 = ([(1,1,1), (2,2,2)], "nave1")
-    let nave2 = ([(1,0,0), (0,1,0)], "nave2")
-    let estado1 = ((0,0,0), True)
-    let estado2 = ((0,0,0), True)
+    let nave1 = ([(1, 1, 1), (2, 2, 2)], "nave1")
+    let nave2 = ([(1, 0, 0), (0, 1, 0)], "nave2")
+    let estado1 = ((0, 0, 0), True)
+    let estado2 = ((0, 0, 0), True)
     let result1 = move_varios_atualizado [nave1, nave2] [estado1, estado2]
-    let expected1 = [(((3,3,3), True), "nave1"), (((1,1,0), True), "nave2")]
+    let expected1 = [(((3, 3, 3), True), "nave1"), (((1, 1, 0), True), "nave2")]
     runTest "Move Varios Atualizado - Basic Movement" (result1 == expected1)
 
     -- Test 2: Collision detection
-    let nave3 = ([(1,1,1)], "nave3")
-    let nave4 = ([(1,1,1)], "nave4")
-    let estado3 = ((0,0,0), True)
-    let estado4 = ((1,1,1), True)
+    let nave3 = ([(2, 2, 2)], "nave3")
+    let nave4 = ([(1, 1, 1)], "nave4")
+    let estado3 = ((0, 0, 0), True)
+    let estado4 = ((1, 1, 1), True)
     let result2 = move_varios_atualizado [nave3, nave4] [estado3, estado4]
-    let expected2 = [(((0,0,0), True), "nave3"), (((1,1,1), True), "nave4")]
+    let expected2 = [(((0, 0, 0), True), "nave3"), (((1, 1, 1), True), "nave4")]
+    putStrLn $ "Result 2" ++ show result2
     runTest "Move Varios Atualizado - Collision Detection" (result2 == expected2)
 
     -- Test 3: Empty list of naves
@@ -39,16 +40,17 @@ testMoveVariosAtualizado = do
 
     -- Test 5: Different lengths of input lists
     let result5 = move_varios_atualizado [nave1] [estado1, estado2]
+    putStrLn $ "Result5" ++ show result5
     let expected5 = []
     runTest "Move Varios Atualizado - Different Lengths" (result5 == expected5)
 
     -- Test 6: Multiple naves with no collisions
-    let nave5 = ([(1,1,1)], "nave5")
-    let nave6 = ([(2,2,2)], "nave6")
-    let estado5 = ((0,0,0), True)
-    let estado6 = ((0,0,0), True)
+    let nave5 = ([(1, 1, 1)], "nave5")
+    let nave6 = ([(2, 2, 2)], "nave6")
+    let estado5 = ((0, 0, 0), True)
+    let estado6 = ((0, 0, 0), True)
     let result6 = move_varios_atualizado [nave5, nave6] [estado5, estado6]
-    let expected6 = [(((1,1,1), True), "nave5"), (((2,2,2), True), "nave6")]
+    let expected6 = [(((1, 1, 1), True), "nave5"), (((2, 2, 2), True), "nave6")]
     runTest "Move Varios Atualizado - Multiple Naves No Collisions" (result6 == expected6)
 
 -- Main function to run all tests
